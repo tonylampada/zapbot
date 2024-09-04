6e0f357eb86d06cf6dc98db54dc3928cf80206da
Jarbas <jarbas@tonylampada.com.br>
2024-09-03 23:29:03 -0300

    meh

--- scripts/start_wpp_connect.sh
+++ scripts/start_wpp_connect.sh
@@ -2,5 +2,5 @@ docker run -d --rm \
     -p 21465:21465 \
     --name wppconnect-server \
     -v $(pwd)/docker/wppconfig.js:/usr/src/wpp-server/dist/config.js \
-    -v $(pwd)/wppconnect_server:/usr/src/wpp-server/tokens \
+    -v $(pwd)/wppconnect_tokens:/usr/src/wpp-server/tokens \
     wppconnect-server
